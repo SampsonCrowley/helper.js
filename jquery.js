60a30e8339d62ee33200786da16b81170238cdcf
luke-schleicher <luke.j.schleicher@gmail.com>
2017-01-04 12:11:56 -0600

    adding jQuery ready

--- jquery.js
+++ jquery.js
@@ -1,4 +1,4 @@
-var jQuery = function(selector){
+var jQuery = function(selector = null){
 
   if(!(this instanceof jQuery)){
     return new jQuery(selector);
@@ -10,29 +10,42 @@ var jQuery = function(selector){
     console.log("by tag");
     this.elem = document.getElementsByTagName(tag);
     return this.elem;
-  }
+  };
 
   this.getByClass = function(className){
     console.log("by class");
     this.elem = document.getElementsByClassName(className);
     return this.elem;
-  }
+  };
 
   this.getById = function(id){
     console.log("by id");
-    this.elem = document.getElementById(id)
+    this.elem = document.getElementById(id);
     return this.elem;
+  };
+
+  this.ready = function(funct) {
+    document.addEventListener('DOMContentLoaded', funct) ||
+    window.addEventListener('load', funct) ||
+    document.attachEvent('onreadystatechange', funct) ||
+    window.attachEvent('onload', funct);
+  };
+
+  if (selector) {
+    if(selector[0].match(/[a-zA-Z]/)){
+      return this.getElement(selector);
+    } else {
+      return {
+        ".": this.getByClass,
+        "#": this.getById
+      }[selector[0]](selector.slice(1));
+    }
   }
 
-  if(selector[0].match(/[a-zA-Z]/)){
-    return this.getElement(selector);
-  } else {
-    return {
-      ".": this.getByClass,
-      "#": this.getById
-    }[selector[0]](selector.slice(1));
-  }
+};
 
-}
 
-console.log(jQuery('.class'));
+
+jQuery().ready(function() {
+  jQuery('h1');
+});
