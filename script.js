b27867a79ea9119ce684d55b73873aa240510c8e
luke-schleicher <luke.j.schleicher@gmail.com>
2017-01-04 10:48:18 -0600

    anonymous function

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function Foo() {
   this.southernify = function(text){
     console.log(text + ", Y'all!");
     return text + ", Y'all!";
-  }
+  };
 }
 
 function Bar() {
@@ -24,36 +24,54 @@ function Baz() {
   this.southernify = function(text){
     console.log(text + ", Y'all!");
     return text + ", Y'all!";
-  }
+  };
 }
 
 foo = new Foo();
 bar = Bar();
 baz = Baz();
-console.log(baz instanceof Baz);
+// console.log(baz instanceof Baz);
 // console.log(bar instanceof Bar);
 // console.log(foo instanceof Foo);
 // console.log(foo);
 // console.log(bar);
-console.log(baz);
+// console.log(baz);
 
 
-function SimpleObject(arr = []) {
-  if(!(this instanceof Baz)){
+var SimpleObject = function(arr = [], funct = null) {
+
+  if(!(this instanceof SimpleObject)){
     return new SimpleObject(arr);
   }
-  this.collection = [];
-  this.each = function(funct){
-    for(i = 0; i < this.collection.length; i++){
-      funct(this.collection[i]);
-    }
-  }
 
-  if((arr.typeOf(Array) || arr.typeOf(String)) && arr.length > 0){
+  this.collection = [];
+
+  // Instance method of each
+  this.each = function(funct){
+    console.log(this.collection);
+    for(i = 0; i < this.collection.length; i++){
+      funct(this.collection[i], i);
+    }
+    return this.collection;
+  };
+
+  if((typeof arr === 'object' || typeof arr === 'string') && arr.length > 0){
+    console.log('57 ran');
     for(i = 0; i < arr.length; i++){
       this.collection.push(arr[i])
     }
   }
+
 }
 
-s = new SimpleObject([1,2,3])
+// Class method of each
+SimpleObject.each = function(array, funct) {
+  for (var i = 0; i < array.length; i++) {
+    funct(array[i], i);
+  }
+  return array;
+};
+
+s = new SimpleObject([1,2,3]);
+
+console.log(SimpleObject);
