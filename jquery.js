792391a6bcc9eb8c98927d6a1dfc141c260505f8
Sampson Crowley <sampsonsprojects@gmail.com>
2017-01-04 11:39:06 -0700

    elements object

--- jquery.js
+++ jquery.js
@@ -4,31 +4,81 @@ var jQuery = function(selector = null){
     return new jQuery(selector);
   }
 
-  this.elem = null;
+  this.elem = {
+    element: null,
+    elements: [],
+    length: 0,
+    idx: function(i){
+      return this.elements[i];
+    }
+
+  };
 
   this.getElement = function(tag){
     console.log("by tag");
-    this.elem = document.getElementsByTagName(tag);
+    this.elem.elements = document.getElementsByTagName(tag);
+    this.elem.element = this.elem.elements[0];
     return this.elem;
   };
 
   this.getByClass = function(className){
     console.log("by class");
-    this.elem = document.getElementsByClassName(className);
+    this.elem.elements = document.getElementsByClassName(className);
+    this.elem.element = this.elem.elements[0];
     return this.elem;
   };
 
   this.getById = function(id){
     console.log("by id");
-    this.elem = document.getElementById(id);
+    this.elem.elements = [document.getElementById(id)];
+    this.elem.element = this.elem.elements[0];
     return this.elem;
   };
 
   this.ready = function(funct) {
-    document.addEventListener('DOMContentLoaded', funct) ||
-    window.addEventListener('load', funct) ||
-    document.attachEvent('onreadystatechange', funct) ||
-    window.attachEvent('onload', funct);
+
+    // Mozilla, Opera and webkit nightlies currently support this event
+    if ( document.addEventListener ) {
+      // Use the handy event callback
+      document.addEventListener( "DOMContentLoaded", function(){
+        document.removeEventListener( "DOMContentLoaded", arguments.callee, false );
+        funct();
+      }, false );
+
+      // If IE event model is used
+    } else if ( document.attachEvent ) {
+      var d = window.document;
+      var done = false;
+      // only fire once
+      var init = function () {
+        if (!done) {
+          done = true;
+          funct();
+        }
+      };
+      // polling for no errors
+      (function () {
+        try {
+          // throws errors until after ondocumentready
+          d.documentElement.doScroll('left');
+        } catch (e) {
+          setTimeout(arguments.callee, 50);
+          return;
+        }
+        // no errors, fire
+        init();
+      })();
+      // trying to always fire before onload
+      d.onreadystatechange = function() {
+        if (d.readyState == 'complete') {
+          d.onreadystatechange = null;
+          init();
+        }
+      };
+
+    }else {
+      window.addEventListener('load', funct, false);
+    }
   };
 
   if (selector) {
@@ -46,6 +96,9 @@ var jQuery = function(selector = null){
 
 
 
+
+
+console.log(jQuery('h1'));
 jQuery().ready(function() {
-  jQuery('h1');
+  console.log(jQuery('h1'));
 });
